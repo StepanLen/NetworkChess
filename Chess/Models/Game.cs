6b14599c11eefb96f4caf7708f781da8ed9e1a39
StepanLen <len.stepa@gmail.com>
2014-11-23 23:26:46 +0200

    Initial commit.
    Signed-off-by: StepanLen <len.stepa@gmail.com>

---
+++ Chess/Models/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNetOpenAuth.OpenId.Extensions.AttributeExchange;
+
+namespace Chess.Models
+{
+    public class Game
+    {
+        public Guid ID { get; set; }
+        public String Link { get; set; }
+        //public Boolean IsNextMoveWhite { get; set; }
+    }
+}
