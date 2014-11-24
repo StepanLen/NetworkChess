b70478f997b0bb70db3f95b5d2c7e087634f5817
StepanLen <len.stepa@gmail.com>
2014-11-24 14:13:47 +0200

    Added Chess.Data project which contains: Local DB, EDMX, Base Service class, Games repository.

---
+++ Chess.Data/Repositories/GamesRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chess.Data.Services;
+
+namespace Chess.Data.Repositories
+{
+    public class GamesRepository
+    {
+        private readonly GameService _service = new GameService(new ChessDBEntities());
+
+        public Game Get(Int32 id)
+        {
+            return _service.Get(id);
+        }
+
+        public Game Get(Guid gameId)
+        {
+            return _service.Find(g => g.GameId.Equals(gameId));
+        }
+
+        public Game Add(Game game)
+        {
+            return _service.Add(game);
+        }
+
+        public Game Save(Game game)
+        {
+            return _service.Update(game, game.Id);
+        }
+    }
+}
---
+++ Chess.Data/Services/GamesService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chess.Data.Repositories;
+
+namespace Chess.Data.Services
+{
+    public class GameService : BaseService<Game>
+    {
+        public GameService(ChessDBEntities context) : base(context)
+        {
+            
+        }
+    }
+}
